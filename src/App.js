c05cf2e37b601f255dfeafe2091f6b5d6bc57681
luchosr <luchosr@gmail.com>
2021-03-15 15:43:09 +0100

    add several components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import "./App.css";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
 import { useState } from "react";
-function Todo({ todo }) {
-  return <div className="todo">{todo.text}</div>;
-}
+
 function App() {
   const [todos, setTodos] = useState([
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "Build really cool todo app" },
+    { text: "Learn about React", isCompleted: false },
+    { text: "Meet friend for lunch", isCompleted: false },
+    { text: "Build really cool todo app", isCompleted: false },
   ]);
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
